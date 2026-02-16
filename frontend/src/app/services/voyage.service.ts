1021dea9ffb675f702238ab7041019ece81af2d7
PhV-80 <63392347+PhV-80@users.noreply.github.com>
2026-02-16 09:42:20 +0100

    WIP Review

---
+++ frontend/src/app/services/voyage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+export interface Voyage {
+  id?: number;
+  ship: number;
+  ship_name?: string;
+  start_port: string;
+  destination_port: string;
+  start_date: string;
+  end_date: string;
+  created_at?: string;
+  updated_at?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VoyageService {
+  private apiUrl: environment.apiUrl/voyages/;
+  private httpClient = inject(HttpClient);
+
+  getVoyages(): Observable<Voyage[]> {
+    return this.http.get<Voyage[]>(this.apiUrl);
+  }
+}
